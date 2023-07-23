be878745ff6d095ecfbe9b688c2c40483d5360c9
KeranLuciel <202014048@uns.edu.pe>
2023-07-23 08:11:42 -0500

    comienzo

---
+++ ClasificacionBazarAnita/src/main/java/clasificacionbazaranita/ClasificacionBazarAnita.java
@@ -0,0 +1,12 @@
+package clasificacionbazaranita;
+
+import Views.*;
+
+public class ClasificacionBazarAnita {
+
+    public static void main(String[] args) {
+        IniciarSesión iniciarSesion = new IniciarSesión();
+        iniciarSesion.setLocationRelativeTo(null);
+        iniciarSesion.setVisible(true);
+    }
+}
