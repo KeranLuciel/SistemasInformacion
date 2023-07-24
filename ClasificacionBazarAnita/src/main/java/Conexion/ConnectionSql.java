fadae37ccfb8399adf9dfc213a553ab7f40186f9
yolandafloresc14 <yolanda141002@gmail.com>
2023-07-23 23:48:06 -0500

    conexion BD

---
+++ ClasificacionBazarAnita/src/main/java/Conexion/ConnectionSql.java
@@ -0,0 +1,61 @@
+
+package Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class ConnectionSql {
+    private static ConnectionSql instancia;
+    private static String url = "jdbc:sqlserver://";
+    private static String nombreServidor = "localhost";
+    private static String numeroPuerto = "1433";
+    private static String nombreBaseDato = "BazarAnita";
+    private static String nombreUsuario = "sa";
+    private static String clave = "1234";
+    private static String conexion;
+    private static Connection connect;
+    
+    
+    public static Connection getInstancia() {
+        if(connect == null){
+            conectar();
+        }        
+        return connect;
+    }
+    
+    
+    public static void conectar() {
+        conexion = url
+                + nombreServidor + ":"
+                + numeroPuerto + ";"
+                + "databaseName=" + nombreBaseDato;
+        
+        try
+        {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            JOptionPane.showMessageDialog(null,"Driver no encontrado: "+e.getMessage(),"Error de conexión",JOptionPane.ERROR_MESSAGE);
+        }
+        
+        try
+        {
+            connect = DriverManager.getConnection(conexion,nombreUsuario,clave);
+            // Por defecto esta habilitado el modo auto-commit en la conexión            
+            connect.setAutoCommit(false); //Para tener el control en forma manual 
+            JOptionPane.showMessageDialog(null, "Conexión realizada");
+        }
+        catch (SQLException e)
+        {
+            JOptionPane.showMessageDialog(null,"Error de nombre de usuario y/o clave: "+e.getMessage(),"Error de conexión",JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+     public static void desconectar() throws SQLException  {
+	connect.close();
+    }
+    
+}
--- ClasificacionBazarAnita/src/main/java/clasificacionbazaranita/ClasificacionBazarAnita.java
+++ ClasificacionBazarAnita/src/main/java/clasificacionbazaranita/ClasificacionBazarAnita.java
@@ -1,10 +1,16 @@
 package clasificacionbazaranita;
 
+import Conexion.ConnectionSql;
 import Views.*;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class ClasificacionBazarAnita {
-
+    
     public static void main(String[] args) {
+        Connection con = ConnectionSql.getInstancia();
         IniciarSesión iniciarSesion = new IniciarSesión();
         iniciarSesion.setLocationRelativeTo(null);
         iniciarSesion.setVisible(true);
